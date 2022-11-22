3912769ccec4966925ef69cea0fac1c0b93886f0
vihanga-guna <mdvcgunathilaka@gmail.com>
2022-11-22 08:50:43 +0200

    Initial commit

---
+++ src/Monitor.java
@@ -0,0 +1,12 @@
+
+public class Monitor {
+
+	public boolean isOn;
+	
+	public void start() {
+		isOn=true;
+	}
+    public void shutdown() {
+    	isOn=false;
+}
+}
---
+++ src/Monitorapp.java
@@ -0,0 +1,25 @@
+
+public class Monitorapp {
+
+	public static void main(String[] args) {
+		
+		Monitor monitor1=new Monitor();
+		monitor1.start();
+		
+		Monitor monitor2=new Monitor();
+		
+		if(monitor1.isOn) {
+		System.out.println("Monitor is on");}
+		
+		if(monitor2.isOn) {
+			System.out.println("Monitor is on");
+		}
+		else {
+			System.out.println("Monitor is off");	
+		}
+
+	}
+
+	
+    
+}
